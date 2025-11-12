2f17693b0d8e93cdd28c1e55d4d8a1b20903262b
Sindhu Hurakadli <sindhuhurakadli3031@gmail.com>
2025-11-12 21:04:16 +0530

    create header compnonet

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Home from './pages/Home'
 import SignIn from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
-  return <BrowserRouter>
+  return( <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/sign-in' element={<SignIn />}/>
@@ -14,5 +16,5 @@ export default function App() {
     <Route path='/about' element={<About />}/>
     <Route path='/profile' element={<Profile />}/>
   </Routes>
-  </BrowserRouter>
+  </BrowserRouter>)
   }

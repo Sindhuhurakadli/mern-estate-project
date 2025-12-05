9f4c0a6826ce4b702201cb3f65e0a61cad30da95
Sindhu Hurakadli <sindhuhurakadli3031@gmail.com>
2025-12-05 12:50:13 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 mongoose.connect(process.env.MONGO)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
@@ -18,6 +18,7 @@ app.use('/api/auth', authRouter);  // <-- signup will work
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+app.use('/api/user', userRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
+// import { updateUserFailure } from '../../client/src/redux/user/userSlice.js';
+import { updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 // Example route
 router.get('/test', test);
+router.post('/update/:id',verifyToken, updateUser);
 export default router;
